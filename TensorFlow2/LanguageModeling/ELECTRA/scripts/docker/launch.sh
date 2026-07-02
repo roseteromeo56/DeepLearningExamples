3e16bd8fa33102566d2d06288ec66b5e267aa3ec
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-02 16:30:25 +0000

    Quote vars in ELECTRA launch script

--- TensorFlow2/LanguageModeling/ELECTRA/scripts/docker/launch.sh
+++ TensorFlow2/LanguageModeling/ELECTRA/scripts/docker/launch.sh
@@ -18,12 +18,12 @@ NV_VISIBLE_DEVICES=${2:-"all"}
 DOCKER_BRIDGE=${3:-"host"}
 
 docker run -it --rm \
-  --gpus device=$NV_VISIBLE_DEVICES \
-  --net=$DOCKER_BRIDGE \
+  --gpus device="$NV_VISIBLE_DEVICES" \
+  --net="$DOCKER_BRIDGE" \
   --shm-size=1g \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
   --privileged \
   -e LD_LIBRARY_PATH='/workspace/install/lib/' \
-  -v $PWD:/workspace/electra \
-  electra $CMD
+  -v "$PWD":/workspace/electra \
+  electra "$CMD"
