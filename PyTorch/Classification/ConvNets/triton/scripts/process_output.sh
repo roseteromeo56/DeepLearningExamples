00f764c1a74ee15b09bd81d34ef8de28cc9b027d
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-03 12:53:09 +0000

    Quote process_output.sh file argument

--- PyTorch/Classification/ConvNets/triton/scripts/process_output.sh
+++ PyTorch/Classification/ConvNets/triton/scripts/process_output.sh
@@ -3,18 +3,18 @@
 echo "Processing file $1"
 echo "Throughput:"
 
-cat $1 | cut -d ',' -f3
+cat "$1" | cut -d ',' -f3
 
 echo ""
 echo "Average latency: "
-cat $1 | cut -d ',' -f4-10 | sed "s/,/\+/g" | sed "s/.*/scale=2; (\0) \/ 1000/g" | bc
+cat "$1" | cut -d ',' -f4-10 | sed "s/,/\+/g" | sed "s/.*/scale=2; (\0) \/ 1000/g" | bc
 
 echo ""
 echo "p90 latency: "
-cat $1 | cut -d ',' -f12 | sed "s/.*/scale=2; \0 \/ 1000/g" | bc
+cat "$1" | cut -d ',' -f12 | sed "s/.*/scale=2; \0 \/ 1000/g" | bc
 echo ""
 echo "p95 latency: "
-cat $1 | cut -d ',' -f13 | sed "s/.*/scale=2; \0 \/ 1000/g" | bc
+cat "$1" | cut -d ',' -f13 | sed "s/.*/scale=2; \0 \/ 1000/g" | bc
 echo ""
 echo "p99 latency: "
-cat $1 | cut -d ',' -f14 | sed "s/.*/scale=2; \0 \/ 1000/g" | bc
+cat "$1" | cut -d ',' -f14 | sed "s/.*/scale=2; \0 \/ 1000/g" | bc
